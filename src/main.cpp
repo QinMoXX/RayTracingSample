143cd5dd5af568499ce2e5f33c73f4fb8be80d8c
QinMo <994674480@qq.com>
2024-07-31 19:42:08 +0800

    feat: calculate normal

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,29 @@
 
 #include <iostream>
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = center - r.origin();
     auto a = dot(r.direction(), r.direction());
     auto b = -2.0 * dot(r.direction(), oc);
     auto c = dot(oc, oc) - radius*radius;
     auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - std::sqrt(discriminant) ) / (2.0*a);
+    }
 }
 
 color ray_color(const ray& r) {
-    if (hit_sphere(vec3(0,0,-1), 0.5, r))
+    vec3 sphere_loc = vec3(0,0,-1);
+    auto t = hit_sphere(sphere_loc, 0.5, r);
+    if (t > 0.0)
     {
-        return color(1,0,0);
+        vec3 N = unit_vector(r.at(t) - sphere_loc);
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
     }
+
+    //background
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
