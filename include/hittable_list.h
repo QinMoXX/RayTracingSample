dd78126a52c8986faa4883c607f775a115c0212d
QinMo <994674480@qq.com>
2024-07-31 23:46:06 +0800

    feat：命中检测对象列表

---
+++ include/hittable_list.h
@@ -0,0 +1,46 @@
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
+
+#include "hittable.h"
+
+#include <memory>
+#include <vector>
+
+using std::make_shared;
+using std::shared_ptr;
+
+class hittable_list : public hittable {
+  public:
+    /*
+    * 这是hittable的集合
+    * 共享指针允许多个几何体共享一个公共实例
+    */
+    std::vector<shared_ptr<hittable>> objects;
+
+    hittable_list() {}
+    hittable_list(shared_ptr<hittable> object) { add(object); }
+
+    void clear() { objects.clear(); }
+
+    void add(shared_ptr<hittable> object) {
+        objects.push_back(object);
+    }
+
+    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+        hit_record temp_rec;
+        bool hit_anything = false;
+        auto closest_so_far = ray_tmax;
+
+        for (const auto& object : objects) {
+            if (object->hit(r, ray_tmin, closest_so_far, temp_rec)) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                rec = temp_rec;
+            }
+        }
+
+        return hit_anything;
+    }
+};
+
+#endif
