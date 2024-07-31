5033a0f37925df67ce3512fc2869082e56167f52
QinMo <994674480@qq.com>
2024-07-31 17:31:08 +0800

    feat: add blue-to-white gradient background

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,12 @@
 
 #include <iostream>
 
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+}
+
 int main() {
 
     // Image
@@ -52,6 +58,6 @@ int main() {
     }
 
     std::clog << "\rDone.                 \n";
-
     
 }
+
