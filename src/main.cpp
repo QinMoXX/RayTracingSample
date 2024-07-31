383a1d012163b2ed0959481490b00b8864f6b628
QinMo <994674480@qq.com>
2024-07-31 19:20:56 +0800

    feat: add a sphere

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,25 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+    if (hit_sphere(vec3(0,0,-1), 0.5, r))
+    {
+        return color(1,0,0);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
+
 
 int main() {
 
@@ -53,6 +67,9 @@ int main() {
             ray r(camera_center, ray_direction);
 
             color pixel_color = ray_color(r);
+
+            
+
             write_color(std::cout, pixel_color);
         }
     }
