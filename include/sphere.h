f20d5af6b6fea0bd57298ca4e6b72cc24513e6b9
QinMo <994674480@qq.com>
2024-07-31 23:45:39 +0800

    feat：射线检测正反面判定

--- include/sphere.h
+++ include/sphere.h
@@ -36,6 +36,9 @@ public:
         rec.t = root;
         rec.p = r.at(rec.t);
         rec.normal = (rec.p - center) / radius;
+        //判定正反面
+        vec3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
 
         return true;
     }
