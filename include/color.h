50872487cc5dde04dc58f5d241711a3a99360185
QinMo <994674480@qq.com>
2024-08-02 00:23:24 +0800

    feat：线性转伽马空间方法

--- include/color.h
+++ include/color.h
@@ -7,12 +7,18 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component)
+{
+    if (linear_component > 0)
+        return std::sqrt(linear_component);
 
+    return 0;
+}
 
 void write_color(std::ostream& out, const color& pixel_color) {
-    auto r = sqrt(pixel_color.x());
-    auto g = sqrt(pixel_color.y());
-    auto b = sqrt(pixel_color.z());
+    auto r = linear_to_gamma(pixel_color.x());
+    auto g = linear_to_gamma(pixel_color.y());
+    auto b = linear_to_gamma(pixel_color.z());
 
     const interval intensity(0.000, 0.999);
     
@@ -23,5 +29,6 @@ void write_color(std::ostream& out, const color& pixel_color) {
     // Write out the pixel color components.
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
+
 
 #endif
