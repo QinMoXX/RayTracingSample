07a0d6b452200bfa075a74657773e1a25ad298b3
QinMo <994674480@qq.com>
2024-08-01 14:10:23 +0800

    feat：兰伯特漫反射模型

--- include/camera.h
+++ include/camera.h
@@ -97,7 +97,7 @@ class camera {
 
         if (world.hit(r, interval(0.001, infinity), rec)) {
             //随机漫反射
-            vec3 direction = random_on_hemisphere(rec.normal);
+            vec3 direction = rec.normal + random_unit_vector();
             return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
         }
 
