12b5f4aa67f36104f3b85d9eaf6f42aee19ceffc
QinMo <994674480@qq.com>
2024-08-02 00:45:34 +0800

    feat：材质类，兰伯特材质

---
+++ include/material.h
@@ -0,0 +1,40 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+#include "rtweekend.h"
+
+class hit_record;
+
+class material {
+  public:
+    virtual ~material() = default;
+
+    virtual bool scatter(
+        const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+    ) const {
+        return false;
+    }
+};
+
+class lambertian : public material {
+  public:
+    lambertian(const color& albedo) : albedo(albedo) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+    const override {
+        auto scatter_direction = rec.normal + random_unit_vector();
+
+        // 排除极度接近于0的向量，以防糟糕的错误
+        if (scatter_direction.near_zero())
+            scatter_direction = rec.normal;
+
+        scattered = ray(rec.p, scatter_direction);
+        attenuation = albedo;
+        return true;
+    }
+
+  private:
+    color albedo;
+};
+
+#endif
