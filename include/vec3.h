3946486f446908abfe0955e48a68b69ba46de1fe
QinMo <994674480@qq.com>
2024-08-02 10:56:04 +0800

    feat：反射计算

--- include/vec3.h
+++ include/vec3.h
@@ -132,4 +132,9 @@ inline vec3 random_on_hemisphere(const vec3& normal) {
         return -on_unit_sphere;
 }
 
+//反射
+inline vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2*dot(v,n)*n;
+}
+
 #endif
